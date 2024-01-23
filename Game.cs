0717046a701951b6dbf13e5f25791a2f1bace751
Jaku-BB <jaku.bb@outlook.com>
2024-01-24 00:41:53 +0100

    feat: add basic logic

--- Game.cs
+++ Game.cs
@@ -2,26 +2,86 @@ namespace GuitarHero;
 
 public class Game
 {
-    private const int Width = 12;
+    // Make sure it's an odd number
+    private const int Width = 13;
     private const int Height = 8;
-    private readonly (ConsoleKey, ConsoleKey, ConsoleKey) _keys = (ConsoleKey.A, ConsoleKey.S, ConsoleKey.D);
+    private const int ClickableHeight = Height - 2;
+    private readonly int[] _horizontalPositions = { 0, Width / 2, Width - 1 };
+    private readonly bool _isRunning = true;
+    private readonly ConsoleKey[] _keys = { ConsoleKey.A, ConsoleKey.S, ConsoleKey.D };
+    private readonly List<Note> _notes = new();
+    private readonly Random _random = new();
+    private int _loopCount;
 
     public void Initialize()
     {
-        Render();
+        int interval = 1000;
+
+        while (_isRunning)
+        {
+            Update();
+            Thread.Sleep(interval);
+
+            if (_loopCount % 10 == 0)
+                interval = Math.Max(300, interval - 100);
+
+            _loopCount++;
+        }
     }
 
     private void Render()
     {
         Console.Clear();
 
-        for (int index = 0; index < Height; index++)
+        for (int row = 0; row < Height; row++)
         {
-            bool isLastIndex = index == Height - 1;
+            bool isLastIndex = row == Height - 1;
 
-            Utilities.WriteAt(0, index, isLastIndex ? _keys.Item1.ToString() : "|");
-            Utilities.WriteAt(Width / 2 - 1, index, isLastIndex ? _keys.Item2.ToString() : "|");
-            Utilities.WriteAt(Width - 1, index, isLastIndex ? _keys.Item3.ToString() : "|");
+            for (int keyIndex = 0; keyIndex < _keys.Length; keyIndex++)
+                Utilities.WriteAt(_horizontalPositions[keyIndex], row, isLastIndex ? _keys[keyIndex].ToString() : "|");
         }
+
+        for (int index = 0; index < _notes.Count; index++)
+        {
+            Note note = _notes[index];
+            Utilities.WriteAt(note.X, note.Y, "X");
+        }
+    }
+
+    private void GenerateNote()
+    {
+        if (_random.Next(0, 2) != 0) return;
+
+        int index = _random.Next(0, _keys.Length);
+        _notes.Add(new Note(_horizontalPositions[index], _keys[index]));
+    }
+
+    private void MoveNote()
+    {
+        List<Note> noteToRemoveList = new();
+
+        for (int index = 0; index < _notes.Count; index++)
+        {
+            Note note = _notes[index];
+
+            if (note.Y >= ClickableHeight)
+            {
+                noteToRemoveList.Add(note);
+                continue;
+            }
+
+            note.Y++;
+        }
+
+        for (int index = 0; index < noteToRemoveList.Count; index++)
+            _notes.Remove(noteToRemoveList[index]);
+    }
+
+
+    private void Update()
+    {
+        GenerateNote();
+        Render();
+        MoveNote();
     }
 }
