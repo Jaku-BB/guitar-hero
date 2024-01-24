c4a6f66f96b386872d99f9b170c1b2da3cd58f11
Jaku-BB <jaku.bb@outlook.com>
2024-01-24 19:03:00 +0100

    feat: handle key press

--- Game.cs
+++ Game.cs
@@ -7,14 +7,17 @@ public class Game
     private const int Height = 8;
     private const int ClickableHeight = Height - 2;
     private readonly int[] _horizontalPositions = { 0, Width / 2, Width - 1 };
-    private readonly bool _isRunning = true;
     private readonly ConsoleKey[] _keys = { ConsoleKey.A, ConsoleKey.S, ConsoleKey.D };
     private readonly List<Note> _notes = new();
     private readonly Random _random = new();
+    private bool _isRunning = true;
     private int _loopCount;
 
     public void Initialize()
     {
+        Thread keyPressThread = new(HandleKeyPress);
+        keyPressThread.Start();
+
         int interval = 1000;
 
         while (_isRunning)
@@ -41,11 +44,7 @@ public class Game
                 Utilities.WriteAt(_horizontalPositions[keyIndex], row, isLastIndex ? _keys[keyIndex].ToString() : "|");
         }
 
-        for (int index = 0; index < _notes.Count; index++)
-        {
-            Note note = _notes[index];
-            Utilities.WriteAt(note.X, note.Y, "X");
-        }
+        _notes.ForEach(note => Utilities.WriteAt(note.X, note.Y, "X"));
     }
 
     private void GenerateNote()
@@ -56,32 +55,60 @@ public class Game
         _notes.Add(new Note(_horizontalPositions[index], _keys[index]));
     }
 
-    private void MoveNote()
+    private void MoveOrRemoveNotes()
     {
-        List<Note> noteToRemoveList = new();
-
-        for (int index = 0; index < _notes.Count; index++)
+        try
         {
-            Note note = _notes[index];
+            Note lowestNote = _notes.OrderBy(note => note.Y).Last();
 
-            if (note.Y >= ClickableHeight)
-            {
-                noteToRemoveList.Add(note);
-                continue;
-            }
+            if (lowestNote.Y >= ClickableHeight) _notes.Remove(lowestNote);
 
-            note.Y++;
+            _notes.ForEach(note => note.Y++);
         }
+        catch (Exception error)
+        {
+            if (error is InvalidOperationException)
+                return;
 
-        for (int index = 0; index < noteToRemoveList.Count; index++)
-            _notes.Remove(noteToRemoveList[index]);
+            throw;
+        }
     }
 
+    private void HandleKeyPress()
+    {
+        const int interval = 10;
+
+        while (_isRunning)
+        {
+            if (!Console.KeyAvailable) continue;
+
+            ConsoleKey key = Console.ReadKey(true).Key;
+
+            if (!_keys.Contains(key))
+            {
+                End();
+                return;
+            }
+
+            if (_notes.Count == 0) continue;
+
+            Thread.Sleep(interval);
+        }
+    }
+
+
+    private void End()
+    {
+        _isRunning = false;
+
+        Console.Clear();
+        Console.WriteLine("Game over!");
+    }
 
     private void Update()
     {
         GenerateNote();
         Render();
-        MoveNote();
+        MoveOrRemoveNotes();
     }
 }
